b284876c63987c1bda5afff0414eaf922130a31b
FIRST_NAME LAST_NAME <oded@benamotz.com>
2023-07-09 04:02:58 +0300

    Work in progress

--- src/routes/astrofetch/+page.svelte
+++ src/routes/astrofetch/+page.svelte
@@ -1,10 +1,128 @@
 <script>
   import Header from "../../components/Header.svelte";
   import nasaLogo from "../../images/svg/NASA.svg";
+  import rocketImg from "../../images/svg/rocket.svg";
+  let today = new Date().toISOString().split("T")[0];
+  let apiKey = "DEMO_KEY";
+  let startDate = today;
+  let endDate = today;
+  let isImage = true;
+  let noImage = false;
+  let isVideo = false;
+  let data;
+  let currentImg = 0;
+  const apiUrl = new URL("https://api.nasa.gov/planetary/apod");
+  async function paste() {
+    try {
+      const clip = await navigator.clipboard.readText();
+      return clip;
+    } catch (error) {
+      console.error(error);
+    }
+    return null;
+  }
+  async function fetchData(url) {
+    let response;
+    try {
+      response = await fetch(url);
+    } catch {
+      alert("Unknown error.");
+      return false;
+    }
+    if (response.status != 200) {
+      if (response.status == 403) {
+        alert("Invalid API key");
+      } else if (response.status == 429) {
+        alert("Too many requests, try using a different API key");
+      } else {
+        alert("Error. Error code: " + response.status);
+      }
+      return false;
+    }
+    const currentData = await response.json();
+    return currentData;
+  }
+  async function insertImages() {
+    const url = addParams(apiUrl, {
+      api_key: apiKey,
+      start_date: startDate,
+      end_date: endDate,
+    });
+    data = await fetchData(url);
+  }
+  const submit = async () => {
+    await insertImages();
+    console.log("data", data);
+  };
+  function addParams(link, params) {
+    link = new URL(link);
+    let value;
+    for (let key in params) {
+      value = params[key];
+
+      link.searchParams.append(key, value);
+    }
+    return link.toString();
+  }
+  function removeLineBreak(string) {
+    string = string.replace(/\n/g, "");
+    string = string.replace(/\r/g, "");
+    return string;
+  }
+  function getDateAhead(dateString, days) {
+    let date = new Date(dateString);
+    date.setDate(date.getDate() + days);
+    let year = String(date.getFullYear()).padStart(4, "0");
+    let month = String(date.getMonth() + 1).padStart(2, "0");
+    let day = String(date.getDate()).padStart(2, "0");
+
+    return year + "-" + month + "-" + day;
+  }
+  function formatDate(dateStr) {
+    const date = new Date(dateStr);
+    const formatDate = date.toLocaleDateString("en-US", dateOptions);
+    return formatDate;
+  }
+  function validateDates(start, end) {
+    let valid = true;
+    let message;
+    if (start < "1996-6-16") {
+      message = "Starting date cannot be before " + formatDate("1996-06-16");
+      valid = false;
+    }
+    if (start > endDate) {
+      if (start > today) {
+        message = "Starting date cannot be after today";
+      } else if (start > endDate) {
+        message = "Starting date cannot be after the end date";
+      }
+      valid = false;
+    }
+    if (end < startDate) {
+      message = "End date cannot be before the starting date";
+      valid = false;
+    }
+    if (end > today) {
+      message = "End date cannot be after today";
+      valid = false;
+    }
+
+    return { valid: valid, message: message };
+  }
+  const pasteToApi = async () => {
+    apiKey = await paste();
+  };
+  const changeStart = (days) => {
+    const final = getDateAhead(startDate, days);
+    if (!validateDates(startDate, final)["valid"]) {
+      endDate = today;
+    } else {
+      endDate = final;
+    }
+  };
 </script>
 
 <Header />
-
 <main class="bg-dark text-light">
   <section class="p-lg-5 text-center text-lg-start">
     <div class="container">
@@ -33,7 +151,7 @@
         </div>
         <img
           class="img-fluid w-50 d-none d-lg-block"
-          src={nasaLogo}
+          src={rocketImg}
           alt="Nasa Logo"
         />
       </div>
@@ -45,45 +163,70 @@
         <div class="col-md-5 col-lg-6">
           <h2>Pick your start date</h2>
           <input
-            oninput="changeEnd()"
+            bind:value={startDate}
+            max={endDate}
             min="1995-06-16"
             type="date"
-            id="startDate"
             class="form-control"
           />
         </div>
         <div class="col-md-7 col-lg-6 mt-md-0 mt-5">
           <h2>Pick your end date</h2>
           <input
-            oninput="changeStart()"
-            min="1995-06-16"
+            bind:value={endDate}
+            min={startDate}
+            max={today}
             type="date"
-            id="endDate"
             class="form-control"
           />
           <div class="row mt-2">
             <div class="col-auto mb-2">
-              <button class="btn btn-secondary" onclick="changeEndDate(0)">
+              <button
+                class="btn btn-secondary"
+                on:click={() => {
+                  changeStart(0);
+                }}
+              >
                 Set to Start Date
               </button>
             </div>
             <div class="col-auto mb-2">
-              <button class="btn btn-secondary" onclick="changeEndDate(7)">
+              <button
+                class="btn btn-secondary"
+                on:click={() => {
+                  changeStart(7);
+                }}
+              >
                 7 Days
               </button>
             </div>
             <div class="col-auto mb-2">
-              <button class="btn btn-secondary" onclick="changeEndDate(30)">
+              <button
+                class="btn btn-secondary"
+                on:click={() => {
+                  changeStart(30);
+                }}
+              >
                 30 Days
               </button>
             </div>
             <div class="col-auto mb-2">
-              <button class="btn btn-secondary" onclick="changeEndDate(100)">
+              <button
+                class="btn btn-secondary"
+                on:click={() => {
+                  changeStart(100);
+                }}
+              >
                 100 Days
               </button>
             </div>
             <div class="col-auto mb-2">
-              <button class="btn btn-secondary" onclick="changeEndDate(365)">
+              <button
+                class="btn btn-secondary"
+                on:click={() => {
+                  changeStart(365);
+                }}
+              >
                 365 Days
               </button>
             </div>
@@ -95,11 +238,10 @@
         <div class="col-md-5">
           <h2>Nasa API key</h2>
           <input
+            bind:value={apiKey}
             type="text"
-            id="apiKey"
             class="form-control"
             placeholder="API key"
-            value="DEMO_KEY"
           />
           <span class="fs-6 fw-light"
             >Don't have an API key? Either leave it as "DEMO_KEY" or visit
@@ -109,7 +251,7 @@
         </div>
         <div class="col-md-1 mt-4 mt-md-0">
           <h2 class="d-none d-md-block">&nbsp;</h2>
-          <button class="btn btn-secondary w-100" onclick="pasteToApi()">
+          <button class="btn btn-secondary w-100" on:click={pasteToApi}>
             <span>
               <i class="fa-regular fa-clipboard" /><span class="d-md-none">
                 &nbsp;Paste from clipboard</span
@@ -119,7 +261,7 @@
         </div>
         <div class="col-md-6 mt-1 mt-md-0">
           <h2 class="d-none d-md-block">&nbsp;</h2>
-          <button onclick="submit()" class="btn btn-primary w-100">
+          <button on:click={submit} class="btn btn-primary w-100">
             Fetch
           </button>
         </div>
@@ -133,15 +275,27 @@
       </h1>
 
       <div class="vh-sm-75 vh-50">
-        <div class="h-100" id="imageContainer">
-          <img
-            src={nasaLogo}
-            alt="NASA logo"
-            class="img-fluid object-fit-contain h-100 w-100"
-            id="mainImage"
-          />
+        <div class="h-100" hidden={!isImage}>
+          {#if data}
+            {#each data as current, index}
+              {#if current["media_type"] == "image"}
+                <img
+                  src={current["url"]}
+                  alt={current["title"]}
+                  hidden={index == currentImg}
+                />
+              {/if}
+            {/each}
+          {:else}
+            <img
+              src={nasaLogo}
+              alt="NASA logo"
+              class="img-fluid object-fit-contain h-100 w-100"
+              id="mainImage"
+            />
+          {/if}
         </div>
-        <div class="container h-100" id="iFrameContainer" hidden>
+        <div hidden={!isVideo} class="container h-100">
           <iframe
             title="Video"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
@@ -151,7 +305,7 @@
             class="h-100 w-100"
           />
         </div>
-        <div hidden id="noImage" class="container h-100">
+        <div hidden={!noImage} class="container h-100">
           <div class="d-flex align-items-center h-100">
             <div class="row">
               <div class="col-12">
@@ -188,12 +342,12 @@
             </div>
           </div>
           <div class="col-6">
-            <button class="btn btn-primary w-100 fs-5" onclick="prevImg()">
+            <button class="btn btn-primary w-100 fs-5" on:click={currentImg++}>
               Back
             </button>
           </div>
           <div class="col-6">
-            <button class="btn btn-primary w-100 fs-5" onclick="nextImg()">
+            <button class="btn btn-primary w-100 fs-5" on:click={currentImg--}>
               Next
             </button>
           </div>
