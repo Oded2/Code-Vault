d14858f0236f82e7c073031167ea67f933acf654
FIRST_NAME LAST_NAME <oded@benamotz.com>
2023-07-13 14:26:03 +0300

    Contact form in progress

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,67 +1,85 @@
 <script>
   import Header from "../../components/Header.svelte";
-  let name, email;
+  import hrefs from "../../data/hrefs.json";
+  const contactEmail = hrefs["email"]["mailto"];
+  const maxLengthShort = 50;
+  const maxLengthLong = 20000;
+  let email, name, topic, message;
+
+  let emailText;
+  const makeEmail = () => {
+    emailText = `User's email: ${email}\nuser's name: ${name}\n\nUser's message: \n\n${message} `;
+  };
+  $: console.log(emailText);
 </script>
 
-<main class="dark-background text-bg-dark">
+<main>
   <Header activePage="contact" title="Contact" />
   <div class="container">
-    <div class="d-flex-center">
-      <div class="p-5 m-5 text-bg-light w-100">
-        <div class="bg-secondary-subtle shadow p-4">
-          <form>
-            <div class="pb-5 pt-3">
-              <h1 class="text-center">Contact</h1>
+    <form on:submit={makeEmail}>
+      <div class="card shadow-lg my-5">
+        <div class="card-body">
+          <div class="border-bottom">
+            <h1 class="font-google-quicksand fw-bold text-center">Contact</h1>
+          </div>
+          <div class="card-text mt-4">
+            <div class="mt-2">
+              <h4 class="form-label font-google-work-sans">Email</h4>
+              <input
+                placeholder="someone@domain.com"
+                maxlength={maxLengthShort}
+                type="email"
+                class="form-control"
+                bind:value={email}
+                required
+              />
+              <p class="form-text">Incase I need to get back to you.</p>
             </div>
-            <div class="fs-5">
-              <div class="mb-3">
-                <p class="form-label font-google-quicksand fw-500">
-                  Email address
-                </p>
-                <input
-                  type="email"
-                  class="form-control"
-                  bind:value={email}
-                  required
-                />
-                <div class="form-text fs-6">
-                  Incase I need to get back to you.
-                </div>
-              </div>
-              <div class="mb-3">
-                <p class="form-label font-google-quicksand fw-500">Name</p>
-                <input
-                  type="text"
-                  class="form-control"
-                  bind:value={name}
-                  required
-                />
-                <div class="form-text fs-6">
-                  Incase I need to get back to you.
-                </div>
-              </div>
+            <div class="mt-2">
+              <h4 class="form-label font-google-work-sans">Name</h4>
+              <input
+                type="text"
+                maxlength={maxLengthShort}
+                class="form-control"
+                bind:value={name}
+                required
+              />
             </div>
-            <!-- <div class="row px-4">
-            <div class="col-md-12 pb-5 pt-3">
-              <h1 class="font-google-quicksand fw-bold text-center">Contact</h1>
+            <div class="mt-2">
+              <h4 class="form-label font-google-work-sans">Topic</h4>
+              <input
+                type="text"
+                maxlength={maxLengthShort}
+                class="form-control"
+                bind:value={topic}
+                required
+              />
             </div>
-            <div class="col-md-5">
-              <h2 class="font-google-quicksand fw-500">Email</h2>
-              <div class="form-text">So I can contact you in the future.</div>
+            <div class="mt-2">
+              <h4 class="form-label font-google-work-sans">Message</h4>
+              <textarea
+                maxlength={maxLengthLong}
+                cols="30"
+                rows="10"
+                class="form-control"
+                bind:value={message}
+                required
+              />
             </div>
-            <div class="col-md-7">
-              <input type="email" required class="form-control" />
-            </div>
-            <div class="col-md-3">
-              <h2 class="font-google-quicksand fw-500">Name</h2>
-            </div>
-            <div class="col-md-9">
-              <input type="text" required class="form-control" />
-            </div>
-          </div> -->
-          </form>
+          </div>
+        </div>
+        <div class="card-footer d-flex justify-content-center">
+          <button class="btn btn-primary w-75 fs-5 fw-bold" value="Send"
+            >Submit</button
+          >
         </div>
       </div>
-    </div>
+    </form>
   </div>
 </main>
+
+<style>
+  textarea {
+    resize: none;
+  }
+</style>
