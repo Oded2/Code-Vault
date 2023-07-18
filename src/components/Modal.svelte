706dfd6da0e849c303a2ff056824d76b464e4535
FIRST_NAME LAST_NAME <oded@benamotz.com>
2023-07-18 16:50:53 +0300

    Modal should look good on mobile now

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -14,7 +14,7 @@
     class="backdrop d-flex align-items-center justify-content-center"
     on:click|self
   >
-    <div class=" container h-50 bg-light rounded card">
+    <div class=" container bg-light rounded card">
       <div class="card-body">
         <slot />
       </div>
@@ -27,10 +27,13 @@
 
 <style>
   .backdrop {
-    width: 100vw;
-    height: 100vh;
+    width: 100%;
+    height: 100%;
     position: fixed;
     background: rgba(0, 0, 0, 0.8);
     z-index: 1;
+  }
+  .card {
+    min-height: 50%;
   }
 </style>
