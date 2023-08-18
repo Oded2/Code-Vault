1b3f5aa96d409df0513f9a0b85b0ca958eff9ba3
FIRST_NAME LAST_NAME <oded@benamotz.com>
2023-08-18 16:09:58 +0300

    In progress adding new project

--- src/routes/mapNews/+page.svelte
+++ src/routes/mapNews/+page.svelte
@@ -1,9 +1,17 @@
 <script>
   import Header from "../../components/Header.svelte";
+  import hrefs from "../../data/hrefs.json";
+  const title = hrefs["map-news"]["home"]["title"];
+
+  let map = L.map("map", {
+    center: [20.0, 5.0],
+    minZoom: 2,
+    zoom: 2,
+  });
 </script>
 
 <main class="full-background text-bg-dark h-100">
-  <Header />
+  <Header {title} />
   <div class="map-container">
     <iframe
       title="World Map"
@@ -12,9 +20,28 @@
     />
   </div>
   <div class="container my-3">
-    <h1 class="font-google quicksand fw-bold">What is map news?</h1>
+    <h1 class="font-google quicksand fw-bold">
+      What is <span class="text-warning">{title}?</span>
+    </h1>
   </div>
 </main>
+
+<svelte:head>
+  <link
+    rel="stylesheet"
+    type="text/css"
+    href="http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.css"
+  />
+
+  <script
+    type="text/javascript"
+    src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"
+  ></script>
+  <script
+    type="text/javascript"
+    src="http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.js"
+  ></script></svelte:head
+>
 
 <style>
   .map-container {
