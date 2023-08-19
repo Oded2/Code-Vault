6c7ab42e4b7b9777a9a91d2180687e1d4002e637
FIRST_NAME LAST_NAME <oded@benamotz.com>
2023-08-19 19:42:41 +0300

    Added share options

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -4,6 +4,16 @@
   export let directory = null;
   export let isHome = false;
   const newTitle = !title ? "Code Vault" : title + " - Code Vault";
+  function addParams(link, params) {
+    link = new URL(link);
+    let value;
+    for (let key in params) {
+      value = params[key];
+
+      link.searchParams.append(key, value);
+    }
+    return link.toString();
+  }
   let breakpoint;
   let pages = [];
   for (let i in directory) {
@@ -17,6 +27,32 @@
     breakpoint = "lg";
   } else {
     breakpoint = "xl";
+  }
+  const shareParams = {
+    twitter: {
+      text: "Check out Oded's code portfolio at Code Vault!",
+      url: "oded.benamotz.com",
+    },
+    facebook: {
+      u: "oded.benamotz.com",
+    },
+  };
+  const shareLinks = {
+    twitter: addParams(
+      "https://twitter.com/intent/tweet",
+      shareParams["twitter"]
+    ),
+    facebook: addParams(
+      "https://facebook.com/share.php",
+      shareParams["facebook"]
+    ),
+    mail: "mailto:?subject=Check Out Code Vault!&body=Code Vault is Oded's coding portfolio. Check it out at https://oded.benamotz.com".replace(
+      / /g,
+      "%20"
+    ),
+  };
+  function copyLink() {
+    navigator.clipboard.writeText("https://oded.benamotz.com");
   }
 </script>
 
@@ -68,6 +104,55 @@
             class="nav-link"
             class:active={title == hrefs["contact"]["title"]}>Contact</a
           >
+        </li>
+        <li>
+          <div class="btn-group">
+            <button
+              type="button"
+              class="btn btn-primary dropdown-toggle"
+              data-bs-toggle="dropdown"
+            >
+              <i class="fa-solid fa-share-from-square" /> Share
+            </button>
+            <ul class="dropdown-menu text-center">
+              <li>
+                <a
+                  href={shareLinks["twitter"]}
+                  class="dropdown-item fs-1"
+                  target="_blank"
+                  ><i
+                    class="fa-brands fa-x-twitter"
+                    style="color: #000000;"
+                  /></a
+                >
+              </li>
+              <li>
+                <a
+                  href={shareLinks["facebook"]}
+                  class="dropdown-item fs-1"
+                  target="_blank"
+                  ><i
+                    class="fa-brands fa-facebook"
+                    style="color: #1877f2;"
+                  /></a
+                >
+              </li>
+              <li>
+                <a
+                  href={shareLinks["mail"]}
+                  class="dropdown-item fs-1"
+                  target="_blank"><i class="fa-solid fa-envelope" /></a
+                >
+              </li>
+              <li>
+                <button
+                  on:click={copyLink}
+                  class="dropdown-item fs-1"
+                  target="_blank"><i class="fa-solid fa-link" /></button
+                >
+              </li>
+            </ul>
+          </div>
         </li>
       </ul>
     </div>
