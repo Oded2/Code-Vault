96f5f602a5cd6e93f154220725ca35319379c57f
FIRST_NAME LAST_NAME <oded@benamotz.com>
2023-08-23 15:40:56 +0300

    Minor ui changes

--- src/components/funProjects/CardInfo.svelte
+++ src/components/funProjects/CardInfo.svelte
@@ -37,8 +37,8 @@
   }
 </script>
 
-<div class="p-5 mb-4 bg-body-tertiary rounded-3 shadow-lg">
-  <div class="container-fluid py-5">
+<div class="mt-4 bg-body-tertiary rounded-3 shadow-lg">
+  <div class="container-fluid px-5 py-4">
     <h1 class="display-5 fw-bold">How does this work?</h1>
     <h3 class="col-md-8 fs-4">
       <h4>
