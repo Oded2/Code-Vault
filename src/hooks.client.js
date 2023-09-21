baff7bb0ca0cf9c9de29e794ef8a162403c9013c
Oded2 <obenamotz@gmail.com>
2023-09-21 10:54:07 +0300

    In  progress adding sign up

--- src/hooks.client.js
+++ src/hooks.client.js
@@ -1,3 +1,50 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, set, child, get } from "firebase/database";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+  apiKey: GCLOUD,
+  authDomain: "code-vault-c5663.firebaseapp.com",
+  databaseURL:
+    "https://code-vault-c5663-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "code-vault-c5663",
+  storageBucket: "code-vault-c5663.appspot.com",
+  messagingSenderId: "74765715916",
+  appId: "1:74765715916:web:657ef2ac13479130753569",
+  measurementId: "G-FF0CTW4C4G",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export function writeUserData(userId, name, email) {
+  const db = getDatabase(app);
+  set(ref(db, "users/" + userId), {
+    username: name,
+    email: email,
+  });
+}
+
+export function readUserData(userId) {
+  const dbRef = ref(getDatabase(app));
+  get(child(dbRef, `users/${userId}`))
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        console.log(snapshot.val());
+      } else {
+        console.log("No data available");
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
 export function randomNum(min, max) {
   const difference = max - min;
   let rand = Math.floor(Math.random() * difference) + min;
---
+++ src/routes/signup/+page.svelte
@@ -0,0 +1,68 @@
+<script>
+  import Header from "../../components/Header.svelte";
+  import hrefs from "../../data/hrefs.json";
+  let email = "",
+    username = "",
+    name = "",
+    password = "";
+
+  function handleSubmit() {
+    console.log("User Submitted");
+  }
+</script>
+
+<main class="full-background">
+  <Header title={hrefs.signup.title} />
+  <div class="container mb-5">
+    <div class="py-5 text-center">
+      <h1 class="font-google-quicksand fw-bold display-3">Sign Up</h1>
+    </div>
+    <div class="card shadow">
+      <div class="card-header">
+        <span class="font-google-quicksand fw-light"
+          >Sign up for Code Vault</span
+        >
+      </div>
+
+      <form on:submit|preventDefault={handleSubmit}>
+        <div class="card-body fs-4">
+          <div class="mb-3">
+            <label class="form-label" for="userEmail">Email</label>
+            <input class="form-control" type="email" id="userEmail" required />
+          </div>
+          <div class="mb-4">
+            <label for="username" class="form-label">Username</label>
+            <input type="text" class="form-control" id="username" required />
+            <div class="form-text fs-6">
+              Must be unique, and must use latin alphabet.
+            </div>
+          </div>
+          <div class="mb-3">
+            <label class="form-label" for="name">Name</label>
+            <input class="form-control" type="text" id="name" required />
+            <div class="form-text fs-6">
+              Doesn't have to be unique, can use any alphabet.
+            </div>
+          </div>
+          <div class="mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input
+              class="form-control"
+              type="password"
+              id="password"
+              minlength="8"
+              required
+            />
+            <div class="form-text fs-6">Must be at least 8 chracters long.</div>
+          </div>
+        </div>
+        <div class="card-footer">
+          <button
+            class="btn btn-primary w-100 fs-4 font-google-quicksand fw-bold"
+            type="submit">Sign Up</button
+          >
+        </div>
+      </form>
+    </div>
+  </div>
+</main>
